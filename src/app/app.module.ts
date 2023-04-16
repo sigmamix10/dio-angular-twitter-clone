aba3dea6fdef4199dd1eeb89fe548b09bf2df184
Rogerio Alves <sigmamix10@gmail.com>
2023-04-16 11:09:43 -0300

    First commit - Prototipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import {MessagesService} from './services/messages.service';
+import { AvatarComponent } from './components/avatar/avatar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,    
+    TweetComponent,
+    HomeComponent,
+    NewPostComponent,
+    FooterComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    MessagesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/new-post/new-post.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MessagesService } from '../../services/messages.service';
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrls: ['./new-post.component.css']
+})
+export class NewPostComponent {
+  tweetMessage:string = ""
+  tweetMaxLength:number = 240
+  tweetRemain:number = this.tweetMaxLength
+
+  constructor(private service:MessagesService){
+    //
+  }
+
+  sendTweet(){
+    this.service.addMessage(this.tweetMessage)
+    this.tweetMessage = ""
+    this.tweetRemain = this.tweetMaxLength    
+  }
+
+  changeTweet(){
+    this.tweetRemain = this.tweetMaxLength - this.tweetMessage.length
+  }
+
+}
